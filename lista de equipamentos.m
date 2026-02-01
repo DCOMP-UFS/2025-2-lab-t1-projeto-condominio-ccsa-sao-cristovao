7c420828a0cdb04838ecfbef7e3a987db4ad4769
Rafael Gomes <152183635+RafaelGomes8994@users.noreply.github.com>
2026-02-01 20:23:06 -0300

    Add equipment list with details and quantities

---
+++ lista de equipamentos.m
@@ -0,0 +1,24 @@
+1 Nobreak Senoidal 2000 VA / 1400 W )	 Nobreak Senoidal 2000 VA / 1400 W (compartilhado)	"32752798000114-1-000028/2025
+
+"	1,00
+ Switch 48 Portas 	Switch 48 Portas + 4 Portas SFP+ 10GB	00394452000103-1-011715/2025-000002	5,00
+gateway (compartilhado)	Gateway 2 portas Gigabit LAN/WAN, 1 porta Gigabit WAN, 8 portas Gigabit LAN	13167804000121-1-000029/2025-000002	1,00
+ rack (compartilhado)	Rack Painel 6U 19" com tampa em acrílico com fechadura e chave	95684478000194-1-000121/2025-000014	1,00
+AP wi-fi 6	Roteador de alta potência tipo AP com banda de até 6Ghz e suporte a POE	"95684478000194-1-000121/2025-000022
+
+"	5,00
+ Computadores All-in-one	Computador All-in-One 12 núcleos 4,8Ghz, 16GB de RAM DDR4, 512GB NVMe, display Full HD de 23,8pol. placa de rede Gigabit e Wi-Fi6, Bluetooth 5.2	"95684478000194-1-000121/2025-000016
+"	4,00
+computador de alto desempenho	Computador de Alto Desempenho com 8 núcleos físicos 4,6Ghz, 16GB de RAM DDR4, GPU com 8GB de VRAM GDDR6, 512GB NVMe, placa de rede Gigabit integrada	95684478000194-1-000121/2025-000009	12,00
+cabo cat5e	Cabo de rede UTP 4 pares trançados CAT5e, caixa contendo 305m	"95684478000194-1-000121/2025-000006
+"	2,00
+Computadores All-in-One	Desktop All-in-One 24pol - Processador Intel i5-14600, 16GB RAM DDR5, NVMe 1TB	"00509018000113-1-003461/2025-000006
+"	10,00
+cadeiras	Cadeira de escritório estofada giratória com assento anatômico em espuma. Encosto de espaldar médio com espuma anatômica. Apoio de braços com regulagem de altura. Assento e encosto em tecido crepe. 	"11002359000114-1-000097/2024-000001
+"	32,00
+impressoras	Impressora Laser HP-Pro	"00509968000148-1-004107/2025-000001
+"	3,00
+monitores	Monitor Ultrawide 24 polegadas	"09795881000159-1-000006/2025-000001
+"	10,00
+projetor	Projetor multimída	07272636000131-1-000197/2025-000001	3,00
+			89,00
